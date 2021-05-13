b2917a93e28fda6ba65b71340b0bd538859c4aec
Artur <arturruschak1@gmail.com>
2021-05-13 11:22:34 +0300

    Update settings.py

--- DevWikiBackend/settings.py
+++ DevWikiBackend/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+if os.environ.get('GITHUB_WORKFLOW'):
+    SECRET_KEY = 'secret'
+    
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
+  
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
