4344ee9093a07b78f7210c21ea3e0cb9a688ef19
Artur <arturruschak1@gmail.com>
2021-05-17 11:06:28 +0300

    Update settings.py

--- DevWikiBackend/settings.py
+++ DevWikiBackend/settings.py
@@ -23,9 +23,13 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+if os.environ.get('GITHUB_WORKFLOW'):
+    SECRET_KEY='testtesttest'    
+
+   
+SECRET_KEY=env.str('SECRET_KEY')
 
     
-SECRET_KEY=env.str('SECRET_KEY')
 
   
 # SECURITY WARNING: don't run with debug turned on in production!
